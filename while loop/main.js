ee6f62d10682a1cd73e1f9e3f18cc3faf888359c
temulen1211 <87556786+temulen1211@users.noreply.github.com>
2023-02-01 02:01:04 +0800

    Add files via upload

---
+++ while loop/main.js
@@ -0,0 +1,71 @@
+//1.
+
+let num = 1;
+while (num <= 10) {
+    console.log(num);
+    num++;
+};
+console.log('\n');
+
+//2.
+
+let oddNum = 0;
+while(oddNum <= 10){
+  if(oddNum % 2 == 1){
+      console.log(oddNum);
+  }
+   oddNum++;
+};
+console.log('\n');
+ 
+//3. 
+
+let evenNum = 0;
+while(evenNum <= 10){
+    if(evenNum % 2 == 0){
+        console.log(evenNum);
+    }
+     evenNum++;
+};
+console.log('\n');
+
+//4.
+
+let sum = 0;
+let i = 1;
+while(i <= 100) {
+    sum += i;
+    i++;
+};
+console.log(sum);
+console.log('\n');
+
+//5. 
+let N = prompt('100 hurtelh N toog garaas ugnu uu!');
+let summ = 0;
+let j = 1;
+while(j <= N) {
+    summ += j;
+    j++;
+};
+console.log(summ);
+console.log('\n');
+
+//9.
+/*let number[i] = prompt('garaas duriin toog ugnu uu');
+let summary = 0;
+while(true) {
+    summary = (sum += number[i])
+    number[i]++;
+    console.log(summary);
+};*/
+
+
+ 
+
+
+
+
+
+
+
