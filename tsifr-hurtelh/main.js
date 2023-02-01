a7872fd9e6c4158dd102ec826898ca893d238c42
temulen1211 <87556786+temulen1211@users.noreply.github.com>
2023-02-01 16:45:34 +0800

    Add files via upload

---
+++ tsifr-hurtelh/main.js
@@ -0,0 +1,14 @@
+//1.
+
+let n = prompt("garaas N toog ugnu uu!");
+let s = ""
+for(i = 0; i < n; i++) {
+    s += ' ' + i;
+}
+console.log(s);
+
+//2.
+
+for(i = 9; i >= n; i--) {
+    console.log(i);
+};
